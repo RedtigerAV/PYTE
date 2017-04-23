858686db374ffffd8da50e6d3c63de6a67502ed7
Anton Vakhrushin <anton_vakhrushin@mail.ru>
2017-04-23 17:35:45 +0300

    first unnecessary commit

---
+++ Pyte/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Pyte {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Open_Calendar(object sender, RoutedEventArgs e) {
+            CalendarFlyouts.IsOpen = true;
+        }
+
+        private void Lol_Click(object sender, RoutedEventArgs e) {
+            AllTasksFlyout.IsOpen = true;
+        }
+    }
+}
